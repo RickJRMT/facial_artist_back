c34b4283a7250917bd9424bda93ed53a0c5130ac
Ricardo Marcillo <jurimat.julian@gmail.com>
2025-11-17 05:00:42 -0500

    Se implemento algunos cambios en controladores para hacer la edicion en CitaAdmin

--- src/routes/admin.citas.routes.js
+++ src/routes/admin.citas.routes.js
@@ -5,11 +5,13 @@ const CitasController = require('../controllers/citas_admin.controller');
 // Obtener todas las citas con detalles
 router.get('/', async (req, res) => {
     try {
-        const citas = await CitasController.obtenerCitasConDetalles();
+        // Lee param de URL (?includeCliente=true) para JOIN condicional (opcional)
+        const includeCliente = req.query.includeCliente === 'true';
+        const citas = await CitasController.obtenerCitasConDetalles(includeCliente);
         res.json(citas);
     } catch (error) {
         res.status(500).json({ message: 'Error al obtener las citas', error: error.message });
     }
 });
 
-module.exports = router; 
+module.exports = router;
